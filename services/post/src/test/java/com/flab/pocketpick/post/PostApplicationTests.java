a377f41579dd42d159e32d223cd384416b6ed9f9
허세진 <heosejin222@gmail.com>
2026-08-19 12:33:02 +0900

    feat: post service 초기 구조 설정 및 yaml 프로파일 분리

---
+++ services/post/src/test/java/com/flab/pocketpick/post/PostApplicationTests.java
@@ -0,0 +1,12 @@
+package com.flab.pocketpick.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
